dfd25a6811949631f80881d15f99797dcb77be94
Hermes <a28704@svalero.com>
2026-05-15 10:55:35 +0200

    Se añade base de datos inicial mariadb

--- src/main/java/com/svalero/equipo5/config/AppConfig.java
+++ src/main/java/com/svalero/equipo5/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.svalero.equipo5.config;
 
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,5 +14,13 @@ public class AppConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return modelMapper;
     }
 }

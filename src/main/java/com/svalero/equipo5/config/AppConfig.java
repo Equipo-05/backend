c52aae6dcf4e049d664594b4b894c5eae1728142
Hermes <a28704@svalero.com>
2026-05-15 11:52:16 +0200

    fix: Se puede lanzar la base de datos, chocaba con autenticación

--- src/main/java/com/svalero/equipo5/config/AppConfig.java
+++ src/main/java/com/svalero/equipo5/config/AppConfig.java
@@ -1,19 +1,33 @@
 package com.svalero.equipo5.config;
 
+import com.svalero.equipo5.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 public class AppConfig {
+
+    private final UserRepository userRepository;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return dni -> (UserDetails) userRepository.findByDni(dni);
     }
 
     @Bean
@@ -23,4 +37,15 @@ public class AppConfig {
 
         return modelMapper;
     }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
 }
--- src/main/java/com/svalero/equipo5/domain/Grants.java
+++ src/main/java/com/svalero/equipo5/domain/Grants.java
@@ -1,4 +1,33 @@
 package com.svalero.equipo5.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "grant")
 public class Grants {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String name;
+    @Column
+    private String type;
+    @Column
+    private String description;
+    @Column
+    private int vacancies;
+    @Column
+    private boolean available;
+    @Column(name = "internal_code")
+    private String internalCode;
+    @Column(name = "created_at")
+    private LocalDate createdAt;
 }

06daf034f5809ad9f35d0d56c332ad0d2e55b61c
Hermes <a28704@svalero.com>
2026-05-15 09:55:36 +0200

    Primer commit

---
+++ src/main/java/com/svalero/equipo5/Equipo5Application.java
@@ -0,0 +1,13 @@
+package com.svalero.equipo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/svalero/equipo5/Equipo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.equipo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Equipo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

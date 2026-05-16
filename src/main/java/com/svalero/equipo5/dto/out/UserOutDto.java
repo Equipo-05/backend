fb3d2a7744e574f85081e53e0851a56ffc767c5b
kyrrz <1999alvaroreyes@gmail.com>
2026-05-16 15:04:55 +0200

    fix: login dto con validacion

--- src/main/java/com/svalero/equipo5/dto/out/UserOutDto.java
+++ src/main/java/com/svalero/equipo5/dto/out/UserOutDto.java
@@ -24,6 +24,7 @@ public class UserOutDto {
     private LocalDate birthDate;
     private Float annualSalary;
     private boolean active;
+    private String role;
 
     @JsonBackReference
     private List<Request> grantRequests;
